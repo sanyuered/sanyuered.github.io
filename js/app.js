ded5114bade6a79337080b437e4d53a3af06b6a9
Liu Fei <la2020@live.com>
2016-09-02 00:58:03 +0800

    index.html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,42 @@
 'use strict';
 
-require(['core'],function(core){
+require(['core'], function (core) {
 
-    core.dom.on("#btnSignIn","click",function(){
-        core.url.navTo("index.html");
-    });
+    var pageName = core.url.getPageName();
+
+    if (pageName == "/index.html") {
+
+        var userInfo = core.data.session.get("userInfo");
+
+        if (userInfo) {
+            core.dom.get("#divWelcome").innerHTML = `Welcome ${userInfo.userName} !`;
+        }
+        else {
+            core.url.navTo("login.html");
+        }
+    }
+    else if (pageName == "/login.html") {
+
+        core.dom.on("#btnSignIn", "click", function () {
+
+            var txtUserName = core.dom.get("#txtUserName").value;
+
+            if (txtUserName) {
+                core.data.session.set("userInfo", {
+                    "userName": txtUserName
+                });
+
+                core.url.navTo("index.html");
+            }
+            else {
+                core.dom.get("#txtUserName").parentNode.parentNode.className = "row-form warning";
+            }
+
+        });
+    }
+
+
+
 
 });
 
--- js/core.js
+++ js/core.js
@@ -1,20 +1,69 @@
 'use strict';
 
-define(function(){
-    var core = {};
-    core.url = {};
+define(function () {
+    var core = core || {};
+    core.data = {};
     core.dom = {};
+    core.url = {};
+    core.data.local = {};
+    core.data.session = {};
 
-    core.url.navTo = function(targetUrl){
+    core.data.local.set = function (key, value) {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    core.data.local.get = function (key) {
+        return JSON.parse(window.localStorage.getItem(key));
+    }
+
+    core.data.local.clear = function () {
+        window.localStorage.clear();
+    }
+
+    core.data.session.set = function (key, value) {
+        window.sessionStorage.setItem(key, JSON.stringify(value));
+    }
+
+    core.data.session.get = function (key) {
+        return JSON.parse(window.sessionStorage.getItem(key));
+    }
+
+    core.data.session.clear = function () {
+        window.sessionStorage.clear();
+    }
+
+    core.dom.get = function (element) {
+        return document.querySelector(element);
+    }
+
+    core.dom.getAll = function (element) {
+        return document.querySelectorAll(element);
+    }
+
+    core.dom.on = function (element, eventName, callback) {
+        var target = core.dom.get(element);
+
+        if (target != null) {
+
+            target.addEventListener(eventName, callback);
+        }
+
+    }
+
+    core.dom.off = function (element, eventName) {
+        var target = core.dom.get(element);
+        if (target != null) {
+            core.dom.get(element).removeEventListener(eventName);
+        }
+    }
+
+    core.url.navTo = function (targetUrl) {
         window.location = targetUrl;
     }
 
-    core.dom.on = function(element,eventName,callback){
-        document.querySelector(element).addEventListener(eventName,callback);
-    }
-
-    core.dom.off = function(element,eventName){
-        document.querySelector(element).removeEventListener(eventName);
+    core.url.getPageName = function () {
+        var href = window.location.href;
+        return href.substr(href.lastIndexOf("/"));
     }
 
     return core;
